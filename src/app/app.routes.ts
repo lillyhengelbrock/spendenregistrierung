c53618833c31e42d84c913a8a170978a9ce745de
lillyhengelbrock <142220672+lillyhengelbrock@users.noreply.github.com>
2025-05-01 09:27:59 +0200

    set up routing, header and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { FormComponent } from './pages/form/form.component';
+import { SummaryComponent } from './pages/summary/summary.component';
+import { LegalComponent } from './pages/legal/legal.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LandingComponent},
+    {path: 'form', component: FormComponent},
+    {path: 'summary', component: SummaryComponent},
+    {path: 'legal', component: LegalComponent},
+    {path: 'privacy', component: PrivacyComponent}
+];
