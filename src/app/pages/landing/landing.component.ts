67db06423baa8d79777a9c0c7db49a07025f2697
lillyhengelbrock <142220672+lillyhengelbrock@users.noreply.github.com>
2025-05-30 21:38:38 +0200

    add missing button on landing page

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -7,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
+  constructor(private router: Router){
 
+  }
+  goBack() {
+    this.router.navigate(['/form']);
+  }
 }
