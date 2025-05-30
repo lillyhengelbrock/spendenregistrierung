6ab28d22ec65e281bded17f06df7cbcc2e3875a1
lillyhengelbrock <142220672+lillyhengelbrock@users.noreply.github.com>
2025-05-30 20:38:28 +0200

    fix formatting, errors and improve styling

--- src/app/pages/summary/summary.component.ts
+++ src/app/pages/summary/summary.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
 import { FormDataService } from '../../services/form-data.service';
 import { SubmissionData } from '../../services/web-api.service';
-import { JsonPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-summary',
-  imports: [JsonPipe],
+  imports: [CommonModule],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.css'
 })
 export class SummaryComponent {
   data: SubmissionData | null = null;
-  constructor (private formDataService: FormDataService){
+  constructor (private formDataService: FormDataService, private router: Router){
     this.data = this.formDataService.getFormData();
     
   }
-  
+  goBack() {
+    this.router.navigate(['/form']);
+  }
 
 }
